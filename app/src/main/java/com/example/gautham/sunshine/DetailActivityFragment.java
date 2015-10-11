c08b2c8b4ca1395203b6d067bf7d424430c41684
OverratedGman <k00691@reading-school.co.uk>
2015-10-11 01:33:23 +0100

    Problems with test scripts

---
+++ app/src/main/java/com/example/gautham/sunshine/DetailActivityFragment.java
@@ -0,0 +1,62 @@
+package com.example.gautham.sunshine;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DetailActivityFragment extends Fragment {
+    private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+    private String mForecastStr;
+
+    public DetailActivityFragment() {
+        setHasOptionsMenu(true);
+
+    }
+
+    private void onCreateMenuItems(Menu menu_main, MenuInflater inflater){
+        inflater.inflate(R.menu.detailfragment,menu_main);
+        MenuItem menuItem = menu_main.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if(mShareActionProvider!=null){
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {Log.d(LOG_TAG,"Share action provider is null?");}
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Intent intent = getActivity().getIntent();
+        View rootV = inflater.inflate(R.layout.fragment_detail, container, false);
+        if(intent!= null && intent.hasExtra(Intent.EXTRA_TEXT))
+        {
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ( (TextView) rootV.findViewById(R.id.detail_text)).setText(mForecastStr);
+
+
+        }
+        return rootV;
+    }
+    private Intent createShareForecastIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
+}
---
+++ app/src/main/java/com/example/gautham/sunshine/SettingsActivityFragment.java
@@ -0,0 +1,19 @@
+package com.example.gautham.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends Fragment {
+
+    public SettingsActivityFragment() {
+    }
+
+
+}
