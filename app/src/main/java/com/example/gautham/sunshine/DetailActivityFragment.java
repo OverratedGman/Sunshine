22b536a9dcf250c741832e8527b7a537a889dc62
OverratedGman <k00691@reading-school.co.uk>
2015-10-18 18:00:50 +0100

    lesson 5

--- app/src/main/java/com/example/gautham/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/gautham/sunshine/DetailActivityFragment.java
@@ -1,20 +1,4 @@
 package com.example.gautham.sunshine;
-
-import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.widget.ShareActionProvider;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
